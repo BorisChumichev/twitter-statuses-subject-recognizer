4ddc3f8ea0116dd45f1b50728dcb01496ba4d769
Boris Chumichev <boris362@yandex.ru>
2014-05-18 00:08:16 +0400

    API

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
+//API
 'use strict'
-var AugmentedTweet = require('./lib/AugmentedTweet.js')
-  , Subject = require('./lib/Subject.js')
-  , SubjectsCollection = require('./lib/SubjectsCollection.js')
-  , TwitterStatusesSubjectRecognizer = require('./lib/TwitterStatusesSubjectRecognizer.js');
-
-module.exports.TwitterStatusesSubjectRecognizer = TwitterStatusesSubjectRecognizer;
-module.exports.AugmentedTweet = AugmentedTweet;
-module.exports.Subject = Subject;
-module.exports.SubjectsCollection = SubjectsCollection;
+module.exports.TwitterStatusesSubjectRecognizer = require('./lib/TwitterStatusesSubjectRecognizer.js');
